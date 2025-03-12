75cc4c56f622cc481fde566360015177b3c7f42e
James Grieve <jamesonrgrieve@gmail.com>
2025-03-11 19:11:28 -0600

    Network async;

--- src/DotNET-Console-Application/Program.cs
+++ src/DotNET-Console-Application/Program.cs
@@ -1,22 +1,17 @@
 ﻿namespace DotNET_Console_Application;
 
+
+
 class Program
 {
+    static readonly HttpClient client = new HttpClient();
     static async Task Main(string[] args)
     {
-        Task<int> result1 = PerformCalculationAsync(2);
-        Task<int> result2 = PerformCalculationAsync(5);
-        Task<int> result3 = PerformCalculationAsync(3);
-        await Task.WhenAll(result1, result2, result3);
-        Console.WriteLine($"{result1.Result} {result2.Result} {result3.Result}");
-    }
+        HttpResponseMessage response = await client.GetAsync("https://randomuser.me/api/");
+        response.EnsureSuccessStatusCode();
 
-    static async Task<int> PerformCalculationAsync(int seconds)
-    {
-        Console.WriteLine($"Performing calculation taking {seconds}s.");
-        await Task.Delay(seconds * 1000); // Simulate long running task
-        Console.WriteLine($"Performed calculation after {seconds}s.");
-        return 42; // Return some result
+        string responseBody = await response.Content.ReadAsStringAsync();
+        Console.WriteLine(responseBody);
     }
 
 }

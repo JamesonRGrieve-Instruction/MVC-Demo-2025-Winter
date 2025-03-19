04d8a0a60aed3cb0b224f40a81c926bc4a6c6560
James Grieve <jamesonrgrieve@gmail.com>
2025-03-18 19:01:34 -0600

    Reset;

--- src/DotNET-Console-Application/Program.cs
+++ src/DotNET-Console-Application/Program.cs
@@ -6,17 +6,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        string userName = Environment.UserName;
-        string machineName = Environment.MachineName;
-        string osVersion = Environment.OSVersion.ToString();
-        string userFolder = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-        string documentsFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-
-        Console.WriteLine($"User Name: {userName}");
-        Console.WriteLine($"Machine Name: {machineName}");
-        Console.WriteLine($"Operating System: {osVersion}");
-        Console.WriteLine($"User Folder: {userFolder}");
-        Console.WriteLine($"Documents Folder: {documentsFolder}");
 
 
     }

381181609a57d41d37af91e052be730bd50e8e92
James Grieve <jamesonrgrieve@gmail.com>
2025-03-18 20:24:29 -0600

    .env File;

--- src/DotNET-Console-Application/Models/CodeFirstContext.cs
+++ src/DotNET-Console-Application/Models/CodeFirstContext.cs
@@ -1,5 +1,5 @@
 using Microsoft.EntityFrameworkCore;
-
+using DotNetEnv;
 namespace DotNET_Console_Application.Models
 {
     public partial class CodeFirstContext : DbContext
@@ -10,9 +10,17 @@ namespace DotNET_Console_Application.Models
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
+            string envPath = ".env";
+            int tries = 10;
+            while (!File.Exists(envPath) && tries > 0)
+            {
+                tries--;
+                envPath = $"../{envPath}";
+            }
+            Env.Load(envPath);
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlite("Data Source=CodeFirst.db");
+                optionsBuilder.UseSqlite($"Data Source={Environment.GetEnvironmentVariable("DB_NAME")}.db");
             }
         }
 

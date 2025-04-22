011b5aa367df515527604fd2d6a928021827f830
James Grieve <jamesonrgrieve@gmail.com>
2025-04-22 17:45:52 -0600

    Enforce authorization for controller;

--- src/ProjectName/Controllers/CourseController.cs
+++ src/ProjectName/Controllers/CourseController.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using ProjectName.Models;
 
 namespace ProjectName
 {
+    [Authorize]
 
     public class CourseController : Controller
     {

edcf8fdbc81be3ea159ed93cccc41ee74a9b09f0
James Grieve <jamesonrgrieve@gmail.com>
2025-04-28 19:12:10 -0600

    Add context and migrations;

---
+++ src/ProjectName/Migrations/20250429011123_InitialMigration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectName.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "courses",
+                columns: table => new
+                {
+                    course_code = table.Column<string>(type: "TEXT", nullable: false),
+                    user_id = table.Column<string>(type: "TEXT", nullable: false),
+                    name = table.Column<string>(type: "TEXT", nullable: false),
+                    description = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_courses", x => x.course_code);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "courses");
+        }
+    }
+}
--- src/ProjectName/Models/Course.cs
+++ src/ProjectName/Models/Course.cs
@@ -1,9 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace ProjectName.Models;
+[Table("courses")]
 public class Course
 {
+    [Key]
+    [Column("course_code")]
     public string CourseCode { get; set; }
+    [Column("user_id")]
     public string UserID { get; set; }
+    [Column("name")]
     public string Name { get; set; }
+    [Column("description")]
     public string Description { get; set; }
 
 }
